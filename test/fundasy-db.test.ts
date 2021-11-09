c697a55a6eda7e71554144a9c15983321d49b35f
Parker <parkervr@bu.edu>
2021-11-09 03:10:06 +0000

    Initial commit

---
+++ test/fundasy-db.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as FundasyDb from '../lib/fundasy-db-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fundasy-db-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FundasyDb.FundasyDbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
